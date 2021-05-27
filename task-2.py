8da96f8cdfbe64f127997c191a03d5c124fe042b
Ridham Goswami <84763258+Ridham2807@users.noreply.github.com>
2021-05-27 15:42:28 +0530

    Add files via upload

---
+++ task-2.py
@@ -0,0 +1,56 @@
+print('hello world')
+
+a,b,c =5,5.5, 'five'
+print(a, 'is integer value')
+print('the value of b and c is',b,c)
+
+name='Ridham'
+print(name)
+
+d=e=10
+print('value of d and e',d,e)
+
+n2=10.5
+print('n2 is',n2,type(n2))
+
+n2=10
+print('n2 is',n2,type(n2))
+
+n2=10+2j
+print('n2 is',n2,type(n2))
+print('n2 is',isinstance(n2,int))
+
+n2=10+2j
+print('n2 is',n2,type(n2))
+print('n2 is',isinstance(n2,complex))
+
+print(name)
+print(name[2])
+print(name[1:5])
+print(name[3:])
+print(name[:4])
+print(name * 2)
+print('name is' +name)
+
+l1=[1,10,20,20.5,'Ridham']
+print(l1)
+print(type(l1))
+print(l1[1])
+print(l1[1:3])
+print(l1[:2])
+
+l1=(1,10,20,20.5,'Ridham')
+print(l1)
+print(type(l1))
+print(l1[1])
+print(l1[1:3])
+print(l1[:2])
+
+d1={20:'Hello','a1':100,20:200}
+print(d1)
+print(type(d1))
+print(d1['a1'])
+print(d1[20])
+
+
+
